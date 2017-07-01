a73234b9794a3d8e07fcbacd8474cd96ba57566e
Nao Yonashiro <owan.orisano@gmail.com>
2017-07-01 23:55:57 +0900

    feat: implement encrypt_and_sign

--- railscookie.py
+++ railscookie.py
@@ -1,25 +1,38 @@
 from Crypto.Cipher import AES
 from Crypto import Random
 
+import base64
 import hashlib
+import hmac
+from typing import Optional
 
+
+class MessageVerifier(object):
+    def __init__(self, secret: bytes):
+        self.secret = secret
+
+    def generate(self, value: bytes) -> bytes:
+        data = self._encode(value)
+        digest = hmac.new(self.secret, msg=data, digestmod="sha1").hexdigest()
+        return b"--".join([data, digest])
+
+    def _encode(self, data: bytes) -> bytes:
+        return base64.b64encode(data)
 
 class MessageEncryptor(object):
     def __init__(self, secret: bytes, sign_secret: bytes):
         self.secret = secret
         self.sign_secret = sign_secret
-        self.cipher = "AES-256-CBC"
-        self.digest = "SHA1"
+        self.verifier = MessageVerifier(secret)
 
     def encrypt_and_sign(self, value: bytes) -> bytes:
-        pass
+        return self.verifier.generate(self._encrypt(value))
 
     def _encrypt(self, value: bytes) -> bytes:
         cipher = self._new_cipher(self.secret)
-        return cipher.encrypt(value)
+        return b"--".join(map(base64.b64encode, [cipher.encrypt(value), cipher.IV]))
 
-
-    def _new_cipher(self, key, iv=None):
+    def _new_cipher(self, key: bytes, iv: Optional[bytes]=None):
         if iv is None:
             iv = Random.new().read(16)
         return AES.new(key, mode=AES.MODE_CBC, IV=iv)
@@ -28,9 +41,10 @@ class RailsCookie(object):
     def __init__(self, secret_key_base: bytes):
         secret = self._generate_key(secret_key_base, b"encrypted cookie")[:32]
         sign_secret = self._generate_key(secret_key_base, b"signed encrypted cookie")
+        self.encryptor = MessageEncryptor(secret, sign_secret)
 
     def _generate_key(self, secret: bytes, salt: bytes) -> bytes:
-        return hashlib.pbkdf2_hmac("sha1", secret, salt, 64)
+        return hashlib.pbkdf2_hmac("sha1", secret, salt, 1000, dklen=64)
 
     def loads(self, cookie):
         pass
